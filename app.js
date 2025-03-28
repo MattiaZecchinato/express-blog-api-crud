bfc51c44a704fe84c7336146cafa61599849a92c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-28 15:08:05 +0100

    feat: add posts routers

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// routers
+const routers = require('./routers/postsRouter.js');
+
+app.get('/', (req, res) => {
+
+    console.log('base test');
+    res.send('base test');
+});
+
+app.use('/posts', routers);
+
+app.listen(port, () => {
+
+    console.log('listen');
+});
